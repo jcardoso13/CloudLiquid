973f57037d9949a561f82b8e89988172e3422c2e
Noélia Anes <nmoreian@emeal.nttdata.com>
2024-06-20 17:17:21 +0100

    Unit tests Tags

--- Tests/AzureTagTests.cs
+++ Tests/AzureTagTests.cs
@@ -2,23 +2,49 @@
 using CloudLiquid.Tags;
 using DotLiquid;
 using Azure.Storage.Blobs;
-using DotLiquid.Exceptions;
 using Microsoft.Extensions.Logging;
 using Moq;
+using DotLiquid.Exceptions;
+using DotLiquid.NamingConventions;
 
 namespace CloudLiquid.Tests
 {
-   
     public class AzureTagTests
     {
         [Fact]
-        public void TestAzureTag()
+        public void AzureTag_TemplateNotFound()
         {
-            //Template.RegisterTagFactory(new CloudLiquidTagFactory(typeof(AzureTag), "include_azure", logger, blobContainerClient));
-
+            var mockLogger = new Mock<ILogger<AzureTag>>();
             var mockBlobContainerClient = new Mock<BlobContainerClient>();
-            Template.RegisterTagFactory(new CloudLiquidTagFactory(typeof(AzureTag), "include_azure", mockBlobContainerClient.Object));
+            Template.RegisterTagFactory(new CloudLiquidTagFactory(typeof(AzureTag), "include_azure", mockLogger.Object, mockBlobContainerClient.Object));
 
+            var azureTag = new AzureTag(mockLogger.Object, mockBlobContainerClient.Object);
+
+            using var writer = new StringWriter();
+            Assert.Throws<SyntaxException>(() => azureTag.Render(null, writer));
         }
+        [Fact]
+        public void AzureTag_InitializeWithLoggerAndBlob()
+        {
+            var mockLogger = new Mock<ILogger<AzureTag>>();
+            var mockBlobContainerClient = new Mock<BlobContainerClient>();
+            Template.RegisterTagFactory(new CloudLiquidTagFactory(typeof(AzureTag), "include_azure", mockLogger.Object, mockBlobContainerClient.Object));
+
+            var azureTag = new AzureTag(mockLogger.Object, mockBlobContainerClient.Object);
+
+            Assert.NotNull(azureTag);
+        }
+        [Fact]
+        public void AzureTag_InvalidMarkupSyntax()
+        {
+            var mockLogger = new Mock<ILogger<AzureTag>>();
+            var mockBlobContainerClient = new Mock<BlobContainerClient>();
+            Template.RegisterTagFactory(new CloudLiquidTagFactory(typeof(AzureTag), "include_azure", mockLogger.Object, mockBlobContainerClient.Object));
+
+            var azureTag = new AzureTag(mockLogger.Object, mockBlobContainerClient.Object);
+
+            Assert.Throws<SyntaxException>(() => azureTag.Initialize("include_azure", "invalid syntax", new List<string>()));
+        }
+        
     }
 }
--- Tests/IncludeLocalTests.cs
+++ Tests/IncludeLocalTests.cs
@@ -1,18 +1,46 @@
 ﻿using Xunit;
 using CloudLiquid.Tags;
 using DotLiquid;
-using Microsoft.Extensions.Logging;
+using DotLiquid.Exceptions;
+using Moq;
 using DotLiquid.FileSystems;
-
+using Microsoft.Extensions.Primitives;
+using System.Globalization;
 
 namespace CloudLiquid.Tests
 {
     public class IncludeLocalTests
-    {
+    { 
         [Fact]
-        public void TestRender_TemplateNotFound()
+        public void IncludeLocal_InvalidTemplate()
         {
             Template.RegisterTagFactory(new CloudLiquidTagFactory(typeof(IncludeLocal), "include_local"));
+
+            var includeLocal = new IncludeLocal();
+            var writer = new StringWriter();
+
+            Assert.Throws<SyntaxException>(() => includeLocal.Render(null, writer));
+        }
+        [Fact]
+        public void IncludeLocal_InvalidMarkup()
+        {
+            var invalidMarkup = "invalid_markup";
+            Template.RegisterTagFactory(new CloudLiquidTagFactory(typeof(IncludeLocal), "include_local"));
+
+            var includeLocal = new IncludeLocal();
+
+            Assert.Throws<SyntaxException>(() => includeLocal.Initialize("include_local", invalidMarkup, null));
+        }
+        [Fact]
+        public void Initialize_ValidMarkup()
+        {
+            var tagName = "include_local";
+            var markup = "\"template_name\"";
+            var tokens = new List<string> { "template_name" };
+            Template.RegisterTagFactory(new CloudLiquidTagFactory(typeof(IncludeLocal), "include_local"));
+            var includeLocal = new IncludeLocal();
+
+            Assert.Throws<SyntaxException>(() => includeLocal.Initialize(tagName, markup, tokens));
         }
     }
 }
